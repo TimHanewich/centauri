44ac985e9fe810b8faf6bcf25cca8127c708bf35
Tim Hanewich <tahanewich@live.com>
2025-08-03 15:12:58 -0400

    Introduced PC based script

---
+++ research/transceiver/PC.py
@@ -0,0 +1,22 @@
+import serial
+import time
+import random
+
+ser = serial.Serial(port="COM3", baudrate=9600, timeout=5)
+
+last_send = time.time()
+
+while True:
+
+    # read
+    if ser.in_waiting:
+        data = ser.read(ser.in_waiting)
+        print("Received data: " + str(data))
+
+    # write?
+    if (time.time() - last_send) >= 10.0:
+        ToSend:str = "Here is a random number: " + str(random.randint(0, 100)) + "\r\n"
+        ser.write(ToSend.encode())
+        last_send = time.time()
+
+    time.sleep(0.05)
