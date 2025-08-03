78264052e64d8fe4d5d7a37863d05642062db492
Tim Hanewich <tahanewich@live.com>
2025-08-03 15:02:45 -0400

    Transceiver test

---
+++ research/transceiver/main.py
@@ -0,0 +1,27 @@
+import sys
+import time
+import select
+
+while True:
+    
+    # Check if there is data to be received
+    if select.select([sys.stdin], [], [], 0)[0]: # if there is data to read. That expression returns a list of data available to read. In Python, if a list is empty, it returns False. If it has something in it, it returns True
+        
+        # collect all bytes, separarated by line
+        read:bytes = bytes()
+        while not read.endswith("\r\n".encode()): # read until new line
+            read = read + sys.stdin.buffer.read(1) # read 1 byte. yes, it is ok to block on this as the \r\n should be coming in a moment...
+        
+        # Send back confirmation of what we received
+        ToSend:str = "Just received this data: " + str(read) + "\r\n"
+        sys.stdout.write(ToSend.encode())
+    else:
+        ToSend:str = "Nothing received.\r\n"
+        sys.stdout.write(ToSend.encode())
+
+    # send something
+    ToSend:str = str(time.ticks_ms()) + "\r\n"
+    sys.stdout.write(ToSend.encode())
+
+    # wait
+    time.sleep(0.25)
