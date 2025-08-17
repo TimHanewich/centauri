240a1b9944def06bfeb6229adda9a4e84995a5c9
Tim Hanewich <tahanewich@live.com>
2025-08-17 19:35:59 -0400

    Also implemented check for desired throttle, to bottom out all motor speeds

--- src/drone/LL-MCU/main.py
+++ src/drone/LL-MCU/main.py
@@ -433,12 +433,12 @@ try:
         m4_throttle = min(max(m4_throttle, 1000000), 2000000)
         #print(str(time.ticks_ms()) + ": M1: " + str(m1_throttle) + ", M2: " + str(m2_throttle) + ", M3: " + str(m3_throttle) + ", M4: " + str(m4_throttle))
 
-        # Before setting motor throttle values:
-        # If it has been a long time since we received a valid desired rates packet,
-        # as a failsafe, turn all inputs (especially throttle) down to 0!!!!!
-        # this ensures if, for some reason, comms stop coming through, it won't get stuck in a high-throttle position
-        if time.ticks_diff(time.ticks_ms(), drates_last_received_ticks_ms) > 2000: # if we havne't received valid drate data in more than 2 seconds
-            # 1,000,000 nanoseconds = 1 ms, the minumum throttle for an ESC (0% throttle, so no rotation)s
+        # MOTOR SHUTDOWN CONDITIONS (safety)
+        # there are two conditions that would mean, no matter what happened above, ALL FOUR motors should be shut down (0% throttle)
+        # scenario 1: desired throttle is 0%. This is obvious. If throttle is 0%, no motors should move. But, the PID loop is still running so it will still be trying to compensate for rate errors, meaning a motor could go OVER 0% throttle.
+        # scenario 2: it has been a long time since we received a valid desired rates packet. This could be due to an error or something with the controller not sending data or the HL-MCU not sending data... but if this happens, as a failsafe, turn off all motors to prevent them from being stuck on.
+        if desired_throttle == 1000000 or time.ticks_diff(time.ticks_ms(), drates_last_received_ticks_ms) > 2000: # if we havne't received valid drate data in more than 2 seconds
+            # 1,000,000 nanoseconds = 1 ms, the minumum throttle for an ESC (0% throttle, so no rotation)
             m1_throttle = 1000000
             m2_throttle = 1000000
             m3_throttle = 1000000
