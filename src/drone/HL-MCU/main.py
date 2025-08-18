e4e08c40da10b33942cb4a453084634da5f3b56c
Tim Hanewich <tahanewich@live.com>
2025-08-18 15:00:29 -0400

    Else print

--- src/drone/HL-MCU/main.py
+++ src/drone/HL-MCU/main.py
@@ -119,10 +119,12 @@ async def main() -> None:
                 print("LL-MCU ponged!")
                 LLMCU_Ponged = True
                 break
+            else:
+                print("It was not a PONG. Waiting a bit longer for PONG...")
         time.sleep(0.1)
 
     # if the LL MCU did not pong back, fail
-    #LLMCU_Ponged = True # uncomment this to bypass LLMCU needing to be on and communicated with (for testing purposes)
+    LLMCU_Ponged = True # uncomment this to bypass LLMCU needing to be on and communicated with (for testing purposes)
     if LLMCU_Ponged == False:
         print("LLMCU did not pong back. Failing.")
         hc12.send(tools.pack_special_packet("LLMCU no pong") + "\r\n".encode())
