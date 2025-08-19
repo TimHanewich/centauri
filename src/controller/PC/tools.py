d694c74838f5d4ee2017bb13bce28ca29caf4037
Tim Hanewich <tahanewich@live.com>
2025-08-19 13:48:15 -0400

    moved key unpacking functions to PC tools

--- src/controller/PC/tools.py
+++ src/controller/PC/tools.py
@@ -1,3 +1,7 @@
+def shift_uint8_to_int8(byte:int) -> int:
+    """Shifts a value that was stored as a uint8 (0-255) to a int8 (-128 to 127)."""
+    return byte - 128
+
 def pack_control_packet(armed:bool, mode:bool, throttle:float, pitch:float, roll:float, yaw:float) -> bytes:
     """Packs a control packet, including the '\r\n' at the end"""
 
@@ -42,3 +46,38 @@ def pack_control_packet(armed:bool, mode:bool, throttle:float, pitch:float, roll
 
     # return it
     return bytes(ToReturn)
+
+def unpack_status(data:bytes) -> dict:
+    """Unpack status packet received from LL-MCU."""
+
+    # ensure it is long enough. And if it isn't, return None
+    if len(data) < 10:
+        return None
+
+    # we will skip the first byte, the header byte, for now, and assume this is indeed a status packet (that should be checked before this function is used)
+
+    # throttles, as float
+    # throttles are expressed as a single byte, from 0-255, with that range representing throttle range 0-100%
+    m1_throttle:float = data[1] / 255
+    m2_throttle:float = data[2] / 255
+    m3_throttle:float = data[3] / 255
+    m4_throttle:float = data[4] / 255
+
+    # pitch, roll, yaw rates
+    # they are stored as a uint8 that will be shifted to a int8
+    # and that int8 value can be interpreted literally... i.e. a value of -6 would mean -6 degrees per second
+    # (yes that loses some fidelity of decimal points)
+    pitch_rate:int = shift_uint8_to_int8(data[5])
+    roll_rate:int = shift_uint8_to_int8(data[6])
+    yaw_rate:int = shift_uint8_to_int8(data[7])
+
+    # pitch and roll angle
+    # they are stored as a uint8 that will be shifted to a int8
+    # and that int8 value can be interpreted literally... i.e. a value of -6 would mean -6 degrees
+    # (yes that loses some fidelity of decimal points)
+    pitch_angle:int = shift_uint8_to_int8(data[8])
+    roll_angle:int = shift_uint8_to_int8(data[9])
+
+    # return dict
+    return {"m1_throttle": m1_throttle, "m2_throttle": m2_throttle, "m3_throttle": m3_throttle, "m4_throttle": m4_throttle, "pitch_rate": pitch_rate, "roll_rate": roll_rate, "yaw_rate": yaw_rate, "pitch_angle": pitch_angle, "roll_angle": roll_angle}
+
