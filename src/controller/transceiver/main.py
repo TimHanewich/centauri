c807777ba5ff7f106cf04831209d643767bacac1
Tim Hanewich <tahanewich@live.com>
2025-08-28 10:05:21 -0400

    Collect byte by byte...

--- src/controller/transceiver/main.py
+++ src/controller/transceiver/main.py
@@ -67,37 +67,30 @@ rxBuffer_fromHC12:bytearray = bytearray()     # butfer for bytes received from q
 led.on() # turn on LED light
 try:
     while True:
-        
-        # Check if we have received data from the PC that we may need to respond to or pass along to the drone (via HC-12)
-        BytesAvailable:int = len(select.select([sys.stdin], [], [], 0)[0]) # counts the number of bytes available to read. The select expression returns a list of data available to read.
-        if BytesAvailable > 0: # if there is data to read.
-            
-            # collect bytes by new line
-            print(str(BytesAvailable) + " bytes available! Reading now...")
-            data:bytes = sys.stdin.buffer.read(BytesAvailable) # read available bytes
-            print(str(len(data)) + " retrieved.")
-            if data != None and len(data) > 0:
-                rxBuffer_fromPC.extend(data)
 
-            # if we have any new lines worth working on (separator/terminator), handle those now
-            while "\r\n".encode() in rxBuffer_fromPC:
+        # continuously collect data, byte by byte while there is data still available
+        while select.select([sys.stdin], [], [], 0)[0]: # if there is data to read. That expression returns "[sys.stdin]" if there is data to read and "[]" if not. In Python, if a list is empty, it returns False. If it has something in it, it returns True
+            rxBuffer_fromHC12.extend(sys.stdin.buffer.read(1)) # read one byte and append it to the buffer
 
-                # get the line
-                loc:int = rxBuffer_fromPC.find("\r\n".encode())
-                ThisLine:bytes = rxBuffer_fromPC[0:loc+2] # include the \r\n at the end (why we +2)
-                rxBuffer_fromPC = rxBuffer_fromPC[loc+2:] # remove the line
+        # if we have any new lines worth working on (separator/terminator), handle those now
+        while "\r\n".encode() in rxBuffer_fromPC:
 
-                # Handle the line
-                if ThisLine.startswith("TRAN".encode()):
-                    if ThisLine == "TRANPING\r\n".encode():
-                        send_tran_msg("PONG")
-                    elif ThisLine == "TRANSTATUS?\r\n".encode():
-                        send_tran_msg(str(hc12.status)) # hc12.status includes the mode, channel, and power, i.e. "{'mode': 3, 'channel': 1, 'power': 8}"
-                    else: # unknown TRAN message
-                        send_tran_msg("?")
-                else: # it is not a TRAN message, so pass it along to the HC-12 to send it to the quadcopter
-                    #hc12.send(ThisLine) # send all the data. Including the \r\n at the end!
-                    pass
+            # get the line
+            loc:int = rxBuffer_fromPC.find("\r\n".encode())
+            ThisLine:bytes = rxBuffer_fromPC[0:loc+2] # include the \r\n at the end (why we +2)
+            rxBuffer_fromPC = rxBuffer_fromPC[loc+2:] # remove the line
+
+            # Handle the line
+            if ThisLine.startswith("TRAN".encode()):
+                if ThisLine == "TRANPING\r\n".encode():
+                    send_tran_msg("PONG")
+                elif ThisLine == "TRANSTATUS?\r\n".encode():
+                    send_tran_msg(str(hc12.status)) # hc12.status includes the mode, channel, and power, i.e. "{'mode': 3, 'channel': 1, 'power': 8}"
+                else: # unknown TRAN message
+                    send_tran_msg("?")
+            else: # it is not a TRAN message, so pass it along to the HC-12 to send it to the quadcopter
+                #hc12.send(ThisLine) # send all the data. Including the \r\n at the end!
+                pass
 
         # check if we have received data from the HC-12 (something from the drone!) that must be passed along to the PC
         newdata:bytes = hc12.receive() # receive new data. hc12.receive returns b'' (empty bytes) if there is nothing new to be had. Note: I know it can be tempting here just to append hc12.receive() to the buffer every time, and that is what I originally had. However, this is very performanced-prohibitive and results in memory being used up after only a few thousand cycles because each time this happens, a new bytes object has to be made.
