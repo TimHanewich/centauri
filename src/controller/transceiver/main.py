123614858bd06bcf5a4cadd347b5eedd9015fe1b
Tim Hanewich <tahanewich@live.com>
2025-08-28 10:12:17 -0400

    Consolidated

--- src/controller/transceiver/main.py
+++ src/controller/transceiver/main.py
@@ -70,10 +70,7 @@ try:
 
         # continuously collect data, byte by byte while there is data still available
         while select.select([sys.stdin], [], [], 0)[0]: # if there is data to read. That expression returns "[sys.stdin]" if there is data to read and "[]" if not. In Python, if a list is empty, it returns False. If it has something in it, it returns True
-            print("There is data to read!")
-            NewBytes:bytes = sys.stdin.buffer.read(1)
-            print("Read this: " + str(NewBytes))
-            rxBuffer_fromPC.extend(NewBytes) # read one byte and append it to the buffer
+            rxBuffer_fromPC.extend(sys.stdin.buffer.read(1)) # read one byte and append it to the buffer
 
         # if we have any new lines worth working on (separator/terminator), handle those now
         while "\r\n".encode() in rxBuffer_fromPC:
