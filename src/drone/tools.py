d15cb98921516a4223d5a23e82f6b174d85b915f
Tim Hanewich <tahanewich@live.com>
2025-09-07 15:49:14 -0400

    Added tools.py for drone with a ported, but updated and corrected unpacking of control data

---
+++ src/drone/tools.py
@@ -0,0 +1,23 @@
+def unpack_control_packet(data:bytes, into:list[int]) -> bool:
+    """Unpack desired rates packet into throttle, desired pitch rate, desired roll rate, and desired yaw rate, into a preexisting list. Returns True if the unpack was successful, False if it did nto unpack because of the checksum failing to verify."""
+
+    # first, validate checksum
+    selfchecksum:int = 0x00
+    for i in range(9): # first 9 bytes
+        selfchecksum = selfchecksum ^ data[i]
+    if selfchecksum != data[9]: # the 10th byte (9th index position) is the checksum value. if the checksum we calculated did not match the checksum in the data itself, must have been a transmission error. Return nothing, fail.
+        return False
+    
+    # unpack throttle, an unsigned short (uint16)
+    into[0] = data[2] << 8 | data[1]
+
+    # unpack pitch, roll, yaw: all signed shorts (int16)
+    # we subtract 32,768 out of each one to shift it BACK to a int16 from a uint16
+    # if you look at the desired rates pack function the HL-MCU has, it is shifting the int16 values into uint16 values before packing to keep it simple.
+    # So we are undoing it here by shifting it back, so negatives can be preserved!
+    into[1] = (data[3] << 8 | data[4]) - 32768
+    into[2] = (data[5] << 8 | data[6]) - 32768
+    into[3] = (data[7] << 8 | data[8]) - 32768
+
+    # return true to indicate the unpack was successful
+    return True
