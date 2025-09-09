9a2cb7e34d0edd52175e151d4d82fc9537fc96c7
Tim Hanewich <tahanewich@live.com>
2025-09-09 17:27:50 -0400

    Reorganized tools.py

--- src/controller/PC/tools.py
+++ src/controller/PC/tools.py
@@ -1,13 +1,3 @@
-def shift_uint8_to_int8(byte:int) -> int:
-    """Shifts a value that was stored as a uint8 (0-255) to a int8 (-128 to 127)."""
-    return byte - 128
-
-
-
-
-
-
-
 ### PACKING DATA TO SEND TO DRONE (through transceiver)
 
 def pack_control_packet(throttle:float, pitch:float, roll:float, yaw:float) -> bytes:
@@ -58,91 +48,7 @@ def pack_control_packet(throttle:float, pitch:float, roll:float, yaw:float) -> b
 
 
 
-
-
-
-
-
-
-
-
-### UNPACKING DATA THAT COMES IN FROM DRONE (through transceiver)
-
-def unpack_control_status(data:bytes) -> dict:
-    """Unpack status packet received from LL-MCU."""
-
-    # ensure it is long enough. And if it isn't, return None
-    if len(data) < 10:
-        return None
-
-    # we will skip the first byte, the header byte, for now, and assume this is indeed a status packet (that should be checked before this function is used)
-
-    # throttles, as float
-    # throttles are expressed as a single byte, from 0-255, with that range representing throttle range 0-100%
-    m1_throttle:float = data[1] / 255
-    m2_throttle:float = data[2] / 255
-    m3_throttle:float = data[3] / 255
-    m4_throttle:float = data[4] / 255
-
-    # pitch, roll, yaw rates
-    # they are stored as a uint8 that will be shifted to a int8
-    # and that int8 value can be interpreted literally... i.e. a value of -6 would mean -6 degrees per second
-    # (yes that loses some fidelity of decimal points)
-    pitch_rate:int = shift_uint8_to_int8(data[5])
-    roll_rate:int = shift_uint8_to_int8(data[6])
-    yaw_rate:int = shift_uint8_to_int8(data[7])
-
-    # pitch and roll angle
-    # they are stored as a uint8 that will be shifted to a int8
-    # and that int8 value can be interpreted literally... i.e. a value of -6 would mean -6 degrees
-    # (yes that loses some fidelity of decimal points)
-    pitch_angle:int = shift_uint8_to_int8(data[8])
-    roll_angle:int = shift_uint8_to_int8(data[9])
-
-    # return dict
-    return {"m1_throttle": m1_throttle, "m2_throttle": m2_throttle, "m3_throttle": m3_throttle, "m4_throttle": m4_throttle, "pitch_rate": pitch_rate, "roll_rate": roll_rate, "yaw_rate": yaw_rate, "pitch_angle": pitch_angle, "roll_angle": roll_angle}
-
-def unpack_system_status(data:bytes) -> dict:
-    """Unpack a system status packet that contains data such as drone battery level, TF Luna distance, and more."""
-
-    # we will ignore the first byte, the header byte, and just assume the data we have been provided is indeed a system status packet (no need to check here, assume it was done before calling this function)
-
-    # unpack battery voltage
-    rval:int = int.from_bytes(data[1:3])
-    aspor:float = rval / 65535
-    battery_voltage:float = 6.0 + ((16.8 - 6.0) * aspor)
-
-    # unpack TF Luna reading
-    tf_luna_distance:int = int.from_bytes(data[3:5])
-
-    # unpack TF Luna strength
-    tf_luna_strength:int = int.from_bytes(data[5:7])
-
-    # unpack altitude in meters
-    rval = int.from_bytes(data[7:9])
-    aspor = rval / 65535
-    altitude:float = (aspor * 9863.58) - 698.42
-
-    # heading
-    rval = data[9]
-    heading = int(rval * (360 / 255))
-
-    # return!
-    return {"battery_voltage": battery_voltage, "tf_luna_distance": tf_luna_distance, "tf_luna_strength": tf_luna_strength, "altitude": altitude, "heading": heading}
-
-def unpack_special_packet(data:bytes) -> str:
-    """Unpacks a special packet, containing free text"""
-
-    # the first byte is a header, so we can ignore
-
-    # determine how far to go in
-    EndOn:int = len(data)
-    if data.endswith("\r\n".encode()):
-        EndOn = EndOn - 2
-    
-    trb:bytes = data[1:EndOn]
-    ToReturn:str = trb.decode(errors="replace") # transmission errors will be replaced with "�"
-    return ToReturn
+######### UNPACKING DATA FROM THE DRONE ########
 
 def unpack_telemetry(data:bytes) -> dict:
     """Unpacks telemetry packet coming from the drone"""
@@ -162,4 +68,18 @@ def unpack_telemetry(data:bytes) -> dict:
 
     # return
     ToReturn:dict = {"vbat": vbat, "pitch_rate": pitch_rate, "roll_rate": roll_rate, "yaw_rate": yaw_rate, "pitch_angle": pitch_angle, "roll_angle": roll_angle}
+    return ToReturn
+
+def unpack_special_packet(data:bytes) -> str:
+    """Unpacks a special packet, containing free text"""
+
+    # the first byte is a header, so we can ignore
+
+    # determine how far to go in
+    EndOn:int = len(data)
+    if data.endswith("\r\n".encode()):
+        EndOn = EndOn - 2
+    
+    trb:bytes = data[1:EndOn]
+    ToReturn:str = trb.decode(errors="replace") # transmission errors will be replaced with "�"
     return ToReturn
