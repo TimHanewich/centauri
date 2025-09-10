26b56a91f89f1c3f720b378e07899c5d21564331
Tim Hanewich <tahanewich@live.com>
2025-09-09 20:41:11 -0400

    Only attempt to send telemetry if throttle is at 0

--- src/drone/main.py
+++ src/drone/main.py
@@ -306,10 +306,14 @@ try:
             led_last_flickered_ticks_ms = time.ticks_ms()
 
         # is it time to send status (telemetry) over to the remote controller via the HC-12?
-        if time.ticks_diff(time.ticks_ms(), status_last_sent_ticks_ms) >= 1000: # every 1000 ms (1 time per second)
-            tools.pack_telemetry(vbat, pitch_rate // 1000, roll_rate // 1000, yaw_rate // 1000, pitch_angle // 1000, roll_angle // 1000, telemetry_packet) # we divide by 1000 (integer division) to reduce back to a single unit (each is stored 1000x the actual to allow for integer math instead of floating point math)
-            uart_hc12.write(telemetry_packet) # no need to append \r\n to it because the bytearray packet already has it at the end!
-            status_last_sent_ticks_ms = time.ticks_ms() # update last sent time
+        # but ONLY send data if we are NOT armed. While throttle input > 0, we are armed and in flight mode, so more important to divert full HC-12 attention to receiving inputs
+        # We do this because the HC-12 is half duplex, meaning it can only receive or send at one time
+        # attempting to send while receiving means good chance a packet will be missed
+        if input_throttle_uint16 == 0:
+            if time.ticks_diff(time.ticks_ms(), status_last_sent_ticks_ms) >= 1000: # every 1000 ms (1 time per second)
+                tools.pack_telemetry(vbat, pitch_rate // 1000, roll_rate // 1000, yaw_rate // 1000, pitch_angle // 1000, roll_angle // 1000, telemetry_packet) # we divide by 1000 (integer division) to reduce back to a single unit (each is stored 1000x the actual to allow for integer math instead of floating point math)
+                uart_hc12.write(telemetry_packet) # no need to append \r\n to it because the bytearray packet already has it at the end!
+                status_last_sent_ticks_ms = time.ticks_ms() # update last sent time
 
         # check for received data (input data) from the HC-12
         # the data that we receive from the HC-12 could be:
