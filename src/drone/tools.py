a1034a0770587d8347b51cb68c6c5a58c181cfed
Tim Hanewich <tahanewich@live.com>
2025-09-11 09:30:02 -0400

    Removed unneeded into parameter and added test

--- src/drone/tools.py
+++ src/drone/tools.py
@@ -38,7 +38,7 @@ def unpack_control_packet(data:bytes, into:list[int]) -> bool:
     # return true to indicate the unpack was successful
     return True
 
-def unpack_settings_update(data:bytes, into:list[int]) -> dict:
+def unpack_settings_update(data:bytes) -> dict:
     """Unpack settings update into a pre-existing list. Returns a dict with settings values if unpack was successful, None if unsuccessful."""
 
     # Check if it is to short
@@ -68,7 +68,9 @@ def unpack_settings_update(data:bytes, into:list[int]) -> dict:
     # return
     return {"pitch_kp": pitch_kp, "pitch_ki": pitch_ki, "pitch_kd": pitch_kd, "roll_kp": roll_kp, "roll_ki": roll_ki, "roll_kd": roll_kd, "yaw_kp": yaw_kp, "yaw_ki": yaw_ki, "yaw_kd": yaw_kd, "i_limit": i_limit}
 
-
+data = b'\x01\x13\x88\x13\x88\x13\x88\x13\x88\x13\x88\x13\x88\x13\x88\x13\x88\x13\x88\x00\x96\x0c'
+settings:dict = unpack_settings_update(data)
+print(str(settings))
 
 
 
