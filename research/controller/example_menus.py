4a67aa6653f86428907a40a35b9d9521c05e6d70
Tim Hanewich <tahanewich@live.com>
2025-10-07 16:29:01 -0400

    Saved example code for showing menus

---
+++ research/controller/example_menus.py
@@ -0,0 +1,43 @@
+import rich.live
+import rich.table
+import rich.text
+import rich.align
+import keyboard
+import time
+
+with rich.live.Live(refresh_per_second=60, screen=True) as l:
+    while True:
+
+        if keyboard.is_pressed("m"):
+            tbl = rich.table.Table()
+            tbl.add_column("Sample Info")
+            tbl.add_column("Sample Data")
+            tbl.add_row("Battery", "16.8")
+            l.update(tbl)
+        elif keyboard.is_pressed("s"):
+            
+            l.stop() # stop live view
+
+            # wait until s is depressed
+            print("Stop pressing S to enter settings.")
+            while keyboard.is_pressed("s"):
+                time.sleep(0.1)
+
+            print("1 - Update PID settings.")
+            print("2 - Do something else")
+            WTD = input("What do you want to do?")
+            if WTD == "1":
+                kp = input("P gain: ")
+                ki = input("I gain: ")
+                kd = input("D gain: ")
+                print("Got it! " + str(kp) + ", " + str(ki) + ", " + str(kd))
+            elif WTD == "2":
+                print("Ok doing something else.")
+                input("Enter to return")
+            else:
+                print("Huh?")
+                input("Enter to continue.")
+        else:
+            txt = rich.text.Text("Press 'm' for main, 's' for settings.")
+            centered = rich.align.Align.center(txt)
+            l.update(centered)
