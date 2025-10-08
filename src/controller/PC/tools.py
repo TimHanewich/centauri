fe78a5a88715a4b823d6a139002fac4754fd7d52
Tim Hanewich <tahanewich@live.com>
2025-10-08 18:59:40 -0400

    Added function for collecting float

--- src/controller/PC/tools.py
+++ src/controller/PC/tools.py
@@ -137,3 +137,11 @@ def ask_integer(prompt:str) -> int:
             return int(ip)
         except:
             print("Invalid input! Must be an integer.")
+
+def ask_float(prompt:str) -> float:
+    while True:
+        ip:str = rich.prompt.Prompt.ask(prompt)
+        try:
+            return float(ip)
+        except:
+            print("Invalid input! Must be a floating point number.")
