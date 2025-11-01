29d3648e38dbe64bf9528ecd98e974e8fa0cefd5
Tim Hanewich <tahanewich@live.com>
2025-11-01 12:14:52 -0400

    Lowered validation of size of packet for telemetry

--- src/drone/tools.py
+++ src/drone/tools.py
@@ -105,8 +105,8 @@ def pack_telemetry(ticks_ms:int, vbat:int, pitch_rate:int, roll_rate:int, yaw_ra
 
     # ensure the provided bytearray is big enough
     if mode == False: # STREAM
-        if len(into) < 7:
-            raise Exception("Provided bytearray of length " + str(len(into)) + " is too small for packing STREAM telemetry into. Must be at least 7 bytes.")
+        if len(into) < 5:
+            raise Exception("Provided bytearray of length " + str(len(into)) + " is too small for packing STREAM telemetry into. Must be at least 5 bytes.")
     else:
         if len(into) < 17:
             raise Exception("Provided bytearray of length " + str(len(into)) + " is too small for packing STORAGE telemetry into. Must be at least 17 bytes.")
