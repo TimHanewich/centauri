082a78a0f0f954d1d2b926db8b7ab7387917c223
Tim Hanewich <tahanewich@live.com>
2025-10-31 20:56:08 -0400

    Expanded data being extracted

--- src/analysis/tools.py
+++ src/analysis/tools.py
@@ -1,6 +1,14 @@
 def unpack_packet(data:bytes) -> dict:
 
     # extract timestamp (in ms)
-    ticks_ms:int = data[0] << 16 | data[1] << 8 | data[0]
+    ticks_ms:int = data[0] << 16 | data[1] << 8 | data[2]
 
-    return {"ticks_ms": ticks_ms}
+    # extract battery reading
+    vbat:float = 6.0 + ((16.8 - 6.0) * (data[3] / 255))
+
+    # rates
+    pitch_rate:int = data[4] - 128
+    roll_rate:int = data[5] - 128
+    yaw_rate:int = data[6] - 128
+
+    return {"ticks_ms": ticks_ms, "vbat": vbat, "pitch_rate": pitch_rate, "roll_rate": roll_rate, "yaw_rate": yaw_rate}
