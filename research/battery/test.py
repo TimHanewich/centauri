b8d47cb8214cde95ef706877b5ab10d8991bceba
Tim Hanewich <tahanewich@live.com>
2025-11-04 07:15:16 -0500

    Clarified how to 'round up'

--- research/battery/test.py
+++ research/battery/test.py
@@ -10,7 +10,10 @@ b:int = -248065
 for adc_val in adc_vals:
     
     # we can back into a floating point number like so:
+    # the original formula was:     voltage:int = (b + (adc_val * m)) // divisor
+    # however, because of integer division, it will always truncate "the remainder" and thus not round up
+    # a classic fix for this, thanks to GPT-5 telling me, is to add half of the divisor to the numerator before dividing. This aparently makes it round up
     divisor:int = 100_000
-    voltage:int = (b + (adc_val * m)) // divisor
+    voltage:int = ((b + (adc_val * m)) + (divisor // 2)) // divisor
 
     print(str(adc_val) + " = " + str(voltage) + " volts")
