7940c22f86976823117a0bedc630fb87d8688d5d
Tim Hanewich <tahanewich@live.com>
2025-11-04 06:32:19 -0500

    Send via HC12

--- research/battery/test_src/main.py
+++ research/battery/test_src/main.py
@@ -1,8 +1,16 @@
 import time
 import machine
+import HC12
 
+# set up
 vbat_adc = machine.ADC(machine.Pin(26))
 led = machine.Pin("LED", machine.Pin.OUT)
+uart_hc12 = machine.UART(1, tx=machine.Pin(8), rx=machine.Pin(9), baudrate=9600)
+hc12 = HC12.HC12(uart_hc12, 7)
+
+# pulse HC-12
+time.sleep(1.0)
+print("HC-12 pulse: " + str(hc12.pulse))
 
 while True:
 
@@ -10,10 +18,13 @@ while True:
 
     # read
     vbat_u16:int = vbat_adc.read_u16()
+    line:str = str(time.ticks_ms()) + " ms: " + str(vbat_u16) + " ADC reading on GP26"
+    
+    # send
+    hc12.send(line.encode())
 
     # write
     f = open("battery.txt", "at")
-    line:str = str(time.ticks_ms()) + " ms: " + str(vbat_u16) + " ADC reading on GP26"
     print(line)
     f.write(line + "\n")
     f.close()
