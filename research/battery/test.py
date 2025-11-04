fbcce8bd041f2d6a46563b5441482e5eb66291ae
Tim Hanewich <tahanewich@live.com>
2025-11-04 07:17:12 -0500

    More notes on the test

--- research/battery/test.py
+++ research/battery/test.py
@@ -13,6 +13,10 @@ for adc_val in adc_vals:
     # the original formula was:     voltage:int = (b + (adc_val * m)) // divisor
     # however, because of integer division, it will always truncate "the remainder" and thus not round up
     # a classic fix for this, thanks to GPT-5 telling me, is to add half of the divisor to the numerator before dividing. This aparently makes it round up
+    # also: note that we are not dividing by the ORIGINAL scaler of 1,000,000 (that we scaled the m and b values to). That would have been what we are supposed to do if we want the result to be a floating point number (i.e. 16.8 volts)
+    # we do NOT want a floating point number - we want an integer! Because integer math is much faster
+    # So, I integer divide by 100,000 (10x less than 1,000,000) to get a value like 168 instead of 16.8 (10x higher and no decimals to deal with)
+    # So, every voltage value we calculate should be between 60 and 168, inclusive.
     divisor:int = 100_000
     voltage:int = ((b + (adc_val * m)) + (divisor // 2)) // divisor
 
