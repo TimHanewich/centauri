7d1aa57328de115a02f0df84926d4046dfaa345b
Tim Hanewich <tahanewich@live.com>
2025-11-13 14:37:42 -0500

    For now, print failure

--- components/quadcopter/src/main.py
+++ components/quadcopter/src/main.py
@@ -405,6 +405,7 @@ try:
                 # decrement how much of the ProcessBuffer is now occupied since we just "extracted" (processed) a line and then moved everything backward like a conveyer belt
                 ProcessBufferOccupied = ProcessBufferOccupied - LineEndLoc
         except Exception as ex:
+            print("RX FAIL: " + str(ex))
             input_throttle_uint16 = 0
             input_pitch_int16 = 0
             input_roll_int16 = 0
