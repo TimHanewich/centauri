b371a57f717470ac217375387f26286cd81e1962
Tim Hanewich <tahanewich@live.com>
2025-11-29 12:34:24 -0500

    Testing

--- components/controller/2 - Mobile/rpi/src/main.py
+++ components/controller/2 - Mobile/rpi/src/main.py
@@ -25,36 +25,44 @@ controller.init()
 print("Controller #0, '" + controller.get_name() + "' will be used.")
 
 # Declare control input variables
-armed:bool = False         # Controlled by Xbox A & B buttons
-throttle:float = 0.0       # between 0.0 and 1.0
-pitch:float = 0.0          # between -1.0 and 1.0
-roll:float = 0.0           # between -1.0 and 1.0
-yaw:float = 0.0            # between -1.0 and 1.0
+input_a:bool = False
+input_b:bool = False
+input_y:bool = False
+input_x:bool = False
+input_dpad_up:bool = False
+input_dpad_right:bool = False
+input_dpad_down:bool = False
+input_dpad_left:bool = False
+input_right_bumper:bool = False
+input_left_bumper:bool = False
+input_left_stick_x:float = 0.0      # -1.0 to 1.0 for left stick right/left
+input_left_stick_y:float = 0.0      # -1.0 to 1.0 for left stick up/down
+input_right_stick_x:float = 0.0     # -1.0 to 1.0 for right stick right/left
+input_right_stick_y:float = 0.0     # -1.0 to 1.0 for right stick up/down
+input_right_trigger:float = 0.0     # 0.0 to 1.0 for right trigger
+input_left_trigger:float = 0.0      # 0.0 to 1.0 for left trigger
 
+# start reading from it!
+print("NOW READING FROM XBOX CONTROLLER!")
 while True:
 
-    # start reading from it!
-    print("NOW READING FROM XBOX CONTROLLER!")
-    while True:
+    for event in pygame.event.get():
+        if event.type == pygame.JOYAXISMOTION: # it has to do with a variable input, like a joystick or trigger
+            print(str(event.axis))
+            # if event.axis == 5: # right trigger
+            #     input_right_trigger = (event.value + 1.0) / 2.0 # gets it to between 0.0 and 1.0
+            # elif event.axis == 0: # left stick X axis (left/right)
+            #     input_left_stick_x = event.value
+            # elif event.axis == 1: # left stick Y axis (up/down)
+            #     # pushing the left stick forward will result in a NEGATIVE value. While this may seem incorrect at first, it is actually correct... pushing the left stick forward should prompt the quadcopter to pitch down (forward), hence it should be negative!
+            #     input_left_stick_y = event.value
+            # elif event.axis == 3: # right stick X axis (left/right) - will be 3 on linux devices, but another ID number on windows.
+            #     yaw = event.value
+        elif event.type == pygame.JOYBUTTONDOWN: # a button was pressed or depressed
+            print(str(event.button))
+            
+    
+    
 
-        for event in pygame.event.get():
-            if event.type == pygame.JOYAXISMOTION: # it has to do with a variable input, like a joystick or trigger
-                if event.axis == 5: # right trigger
-                    throttle = (event.value + 1.0) / 2.0 # gets it to between 0.0 and 1.0
-                elif event.axis == 0: # left stick X axis (left/right)
-                    roll = event.value
-                elif event.axis == 1: # left stick Y axis (up/down)
-                    # pushing the left stick forward will result in a NEGATIVE value. While this may seem incorrect at first, it is actually correct... pushing the left stick forward should prompt the quadcopter to pitch down (forward), hence it should be negative!
-                    pitch = event.value
-                elif event.axis == 3: # right stick X axis (left/right) - will be 3 on linux devices, but another ID number on windows.
-                    yaw = event.value
-            elif event.type == pygame.JOYBUTTONDOWN: # a button was pressed or depressed
-                if event.button == 0: # A
-                    armed = True
-                elif event.button == 1: # B
-                    armed = False
-        
-        print("ARMED:" + str(armed) + ", Throttle: " + str(throttle) + ", Pitch: " + str(pitch) + ", Roll: " + str(roll) + ", Yaw: " + str(yaw))
-
-        # wait a moment
-        time.sleep(0.05)
+    # wait a moment
+    time.sleep(0.05)
