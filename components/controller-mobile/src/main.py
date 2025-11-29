3ce9c4aa321d3af3df059a7401a9224f1edd2200
Tim Hanewich <tahanewich@live.com>
2025-11-29 08:02:51 -0500

    Set up inputs

--- components/controller-mobile/src/main.py
+++ components/controller-mobile/src/main.py
@@ -24,6 +24,13 @@ controller = pygame.joystick.Joystick(0)
 controller.init()
 print("Controller #0, '" + controller.get_name() + "' will be used.")
 
+# Declare control input variables
+throttle:float = 0.0       # between 0.0 and 1.0
+pitch:float = 0.0          # between -1.0 and 1.0
+roll:float = 0.0           # between -1.0 and 1.0
+yaw:float = 0.0            # between -1.0 and 1.0
+
+
 # start reading from it!
 print("NOW READING FROM XBOX CONTROLLER!")
 while True:
@@ -31,9 +38,18 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.JOYAXISMOTION: # it has to do with a variable input, like a joystick or trigger
             if event.axis == 5: # right trigger
-                print("RT: " + str(event.value))
+                throttle = (throttle + 1.0) / 2.0 # gets it to between 0.0 and 1.0
+            elif event.axis == 0: # left stick X axis (left/right)
+                roll = event.value
+            elif event.axis == 1: # left stick Y axis (up/down)
+                # pushing the left stick forward will result in a NEGATIVE value. While this may seem incorrect at first, it is actually correct... pushing the left stick forward should prompt the quadcopter to pitch down (forward), hence it should be negative!
+                pitch = event.value
+            elif event.axis == 3: # right stick X axis (left/right) - will be 3 on linux devices, but another ID number on windows.
+                yaw = event.value
         elif event.type == pygame.JOYBUTTONDOWN: # a button was pressed or depressed
             pass
     
+    print("Throttle: " + str(throttle) + ", Pitch: " + str(pitch) + ", Roll: " + str(roll) + ", Yaw: " + str(yaw))
+
     # wait a moment
     time.sleep(0.05)
