8a9fa72a265bf477105f0913b15f353ac940db69
Tim Hanewich <tahanewich@live.com>
2025-12-03 18:49:14 -0500

    Went back to old unpack function

--- components/controller/handheld/pico/src/tools.py
+++ components/controller/handheld/pico/src/tools.py
@@ -1,138 +1,106 @@
 import math
 
-### FOR RECEIVING CONTROLLER INPUT ###
+def unpack_controls(data:bytes) -> dict:
+    """Unpacks control data (from a Raspberry Pi w/ a controller connected) to normal data."""
 
-def unpack_button_input(packed:bytes) -> tuple[int, bool]: # returns the int that corresponds to the Button - see the "Button" Enum in the RPi's tools.py (MicroPython doesn't support Enum), and then the status (True = pressed, False = depressed)
-
-    if len(packed) == 0:
+    if len(data) < 14: # while the full packet is normally 16, that is including the \r\n terminator. 14 is the actual data. So it has to be at least 14 in length.
         return None
 
-    # define a binary mask of the bit positions we ONLY want
-    # we only want bits 0, 1, 2, and 3. Those represent the numeric value
-    # bits 4, 5, 6, and 7 are bits that contain other info
-    # We will isolate bits 0,1,2,3 and thus get the numeric value of the button, unaffected by those other info header bits
-    mask:int = 0b00001111
+    ToReturn:dict = {}
 
-    # Determine what button was pressed
-    # this AND operation will only allow the bits that match up to be 1
-    # since we set bit 4-7 to 0, it will be impossible for those to come out as 1
-    btn_id:int = packed[0] & mask
-
-    # Now determine the status (pressed or depressed)
-    # if bit 5 is 1, it is pressed. Otherwise, it is not
-    pressed:bool = packed[0] & 0b00100000 > 0
-
-    return (btn_id, pressed)
-
-def unpack_joystick_input(packed:bytes) -> tuple[int, float]: # unpacks as the ID of the joystick (see 'Joystick' Enum of the RPi's tools.py) and then the value as a floating point number
-
-    # if the length is not at least 3 (one header byte, two value bytes), there is a problem
-    if len(packed) < 3:
-        return None
+    # left stick pressed? (clicked down)
+    if data[0] & 0b00100000 > 0:
+        ToReturn["ls"] = True
+    else:
+        ToReturn["ls"] = False
     
-    # define a binary mask of the bit positions we ONLY want
-    # we only want bits 0, 1, and 2. Those represent the numeric value
-    # bits 3, 4, 5, 6, and 7 are bits that contain other info
-    # We will isolate bits 0,1,2 and thus get the numeric value of the button, unaffected by those other info header bits
-    mask:int = 0b00000111
+    # right stick pressed? (clicked down)
+    if data[0] & 0b00010000 > 0:
+        ToReturn["rs"] = True
+    else:
+        ToReturn["rs"] = False
 
-    # Determine what joystick was moved
-    # this AND operation will only allow the bits that match up to be 1
-    # since we set bit 3-7 to 0, it will be impossible for those to come out as 1
-    joystick_id:int = packed[0] & mask
+    # back button pressed?
+    if data[0] & 0b00001000 > 0:
+        ToReturn["back"] = True
+    else:
+        ToReturn["back"] = False
 
-    # Get the value
-    value:float = 0.0 # start w/ 0
-    if joystick_id == 4 or joystick_id == 5: # if it is the LT or RT, it is between 0.0 and 1.0
-        asuint16:int = (packed[1] << 8) | packed[2]
-        value = asuint16 / 65535 # just a flat % of the total range!
-    else: # otherwise, it is the X/Y axis of the Left/Right stick... which can be between -1.0 and 1.0
-        asuint16:int = (packed[1] << 8) | packed[2]
-        aspor:float = asuint16 / 65535 # % of total range
-        value = (2 * aspor) - 1.0 # restore to a -1.0 to 1.0
+    # start button pressed?
+    if data[0] & 0b00000100 > 0:
+        ToReturn["start"] = True
+    else:
+        ToReturn["start"] = False
 
-    return (joystick_id, value)
+    # a button pressed?
+    if data[0] & 0b00000010 > 0:
+        ToReturn["a"] = True
+    else:
+        ToReturn["a"] = False
 
+    # b button pressed?
+    if data[0] & 0b00000001 > 0:
+        ToReturn["b"] = True
+    else:
+        ToReturn["b"] = False
 
+    # x button pressed?
+    if data[1] & 0b10000000 > 0:
+        ToReturn["x"] = True
+    else:
+        ToReturn["x"] = False
 
+    # y button pressed?
+    if data[1] & 0b01000000 > 0:
+        ToReturn["y"] = True
+    else:
+        ToReturn["y"] = False
 
-##################################################
-### FOR PACKING/UNPACKING TELEMETRY FROM DRONE ###
-### LIFTED DIRECTLY OUT OF THE PC's utils.py   ###
-##################################################
+    # up dpad button pressed?
+    if data[1] & 0b00100000 > 0:
+        ToReturn["up"] = True
+    else:
+        ToReturn["up"] = False
 
-def unpack_telemetry(data:bytes) -> dict:
-    """Unpacks telemetry packet coming from the drone"""
+    # right dpad button pressed?
+    if data[1] & 0b00010000 > 0:
+        ToReturn["right"] = True
+    else:
+        ToReturn["right"] = False
 
-    # if it is not long enough, return None to indicate it didn't work
-    if len(data) < 5:
-        return None
+    # down dpad button pressed?
+    if data[1] & 0b00001000 > 0:
+        ToReturn["down"] = True
+    else:
+        ToReturn["down"] = False
 
-    # the first byte is a header (metadata) byte
+    # left dpad button pressed?
+    if data[1] & 0b00000100 > 0:
+        ToReturn["left"] = True
+    else:
+        ToReturn["left"] = False
 
-    # battery voltage
-    # the battery voltage will come in 10x what it is - so 168 would be 16.8, 60 would be 6.0
-    # so just divide by 10 to get the actual value (as a float)
-    vbat:float = data[1] / 10
+    # left bumper pressed?
+    if data[1] & 0b00000010 > 0:
+        ToReturn["lb"] = True
+    else:
+        ToReturn["lb"] = False
 
-    # rates & angles
-    # we subtract 128 here to "shift back" to a signed byte from an unsigned byte (128 is added before packing it)
-    pitch_rate:int = data[2] - 128
-    roll_rate:int = data[3] - 128
-    yaw_rate:int = data[4] - 128
-    pitch_angle:int = data[5] - 128
-    roll_angle:int = data[6] - 128
+    # right bumper pressed?
+    if data[1] & 0b00000001 > 0:
+        ToReturn["rb"] = True
+    else:
+        ToReturn["rb"] = False
 
-    # return
-    ToReturn:dict = {"vbat": vbat, "pitch_rate": pitch_rate, "roll_rate": roll_rate, "yaw_rate": yaw_rate, "pitch_angle": pitch_angle, "roll_angle": roll_angle}
+    # Axes (variables)
+    ToReturn["left_x"] = -1 + ((data[2] << 8 | data[3]) / 65535) * 2
+    ToReturn["left_y"] = -1 + ((data[4] << 8 | data[5]) / 65535) * 2
+    ToReturn["right_x"] = -1 + ((data[6] << 8 | data[7]) / 65535) * 2
+    ToReturn["right_y"] = -1 + ((data[8] << 8 | data[9]) / 65535) * 2
+    ToReturn["lt"] = (data[10] << 8 | data[11]) / 65535
+    ToReturn["rt"] = (data[12] << 8 | data[13]) / 65535
+
     return ToReturn
-
-def pack_control_packet(throttle:float, pitch:float, roll:float, yaw:float) -> bytes:
-    """
-    Packs a control packet to be sent to the drone.
-    
-    Expects:
-    throttle between 0.0 and 1.0
-    pitch between -1.0 and 1.0
-    roll between -1.0 and 1.0
-    yaw between -1.0 and 1.0
-    """
-
-    ToReturn:bytearray = bytearray()
-
-    # Add header byte (metadata byte)
-    # bit 7, 6, 5, 4 are reserved (unused)
-    header:int = 0b00000000 # start with 0. Bit 0 must be 0 to identify as a control packet, which it already is!s
-    ToReturn.append(header)
-
-    # Add throttle bytes
-    asint16 = min(max(int(round(throttle * 65535, 0)), 0), 65535) # express as number between 0 and 65535
-    ToReturn.extend(asint16.to_bytes(2, "big"))
-
-    # add pitch bytes
-    aspop:float = (pitch + 1) / 2 # as percent of the range -1.0 to 1.0
-    asint16:int = min(max(int(round(aspop * 65535, 0)), 0), 65535) # convert the range from a number between 0 and 65535 (uint16)
-    ToReturn.extend(asint16.to_bytes(2, "big")) # pack as 2-bytes using big endian
-
-    # add roll bytes
-    aspop:float = (roll + 1) / 2 # as percent of the range -1.0 to 1.0
-    asint16:int = min(max(int(round(aspop * 65535, 0)), 0), 65535) # convert the range from a number between 0 and 65535 (uint16)
-    ToReturn.extend(asint16.to_bytes(2, "big")) # pack as 2-bytes using big endian
-
-    # add yaw bytes
-    aspop:float = (yaw + 1) / 2 # as percent of the range -1.0 to 1.0
-    asint16:int = min(max(int(round(aspop * 65535, 0)), 0), 65535) # convert the range from a number between 0 and 65535 (uint16)
-    ToReturn.extend(asint16.to_bytes(2, "big")) # pack as 2-bytes using big endian
-
-    # Add XOR-chain-based checksum
-    checksum:int = 0x00 # start with 0
-    for byte in ToReturn: # for each byte added so far
-        checksum = checksum ^ byte # XOR operation
-    ToReturn.append(checksum)
-
-    # return it
-    return bytes(ToReturn)
-
 
 # Lifted from the Scout Flight Controller, my previous work: https://github.com/TimHanewich/scout/blob/master/src/toolkit.py
 class NonlinearTransformer:
