78f8a3c9a57a6394ca370d381465b60f46cde8f9
Tim Hanewich <tahanewich@live.com>
2025-12-03 18:56:03 -0500

    Re-added back drone related packing/unpacking

--- components/controller/handheld/pico/src/tools.py
+++ components/controller/handheld/pico/src/tools.py
@@ -102,6 +102,97 @@ def unpack_controls(data:bytes) -> dict:
 
     return ToReturn
 
+
+
+
+
+##################################################
+### FOR PACKING/UNPACKING TELEMETRY FROM DRONE ###
+### LIFTED DIRECTLY OUT OF THE PC's utils.py   ###
+##################################################
+
+def unpack_telemetry(data:bytes) -> dict:
+    """Unpacks telemetry packet coming from the drone"""
+
+    # if it is not long enough, return None to indicate it didn't work
+    if len(data) < 5:
+        return None
+
+    # the first byte is a header (metadata) byte
+
+    # battery voltage
+    # the battery voltage will come in 10x what it is - so 168 would be 16.8, 60 would be 6.0
+    # so just divide by 10 to get the actual value (as a float)
+    vbat:float = data[1] / 10
+
+    # rates & angles
+    # we subtract 128 here to "shift back" to a signed byte from an unsigned byte (128 is added before packing it)
+    pitch_rate:int = data[2] - 128
+    roll_rate:int = data[3] - 128
+    yaw_rate:int = data[4] - 128
+    pitch_angle:int = data[5] - 128
+    roll_angle:int = data[6] - 128
+
+    # return
+    ToReturn:dict = {"vbat": vbat, "pitch_rate": pitch_rate, "roll_rate": roll_rate, "yaw_rate": yaw_rate, "pitch_angle": pitch_angle, "roll_angle": roll_angle}
+    return ToReturn
+
+def pack_control_packet(throttle:float, pitch:float, roll:float, yaw:float) -> bytes:
+    """
+    Packs a control packet to be sent to the drone.
+    
+    Expects:
+    throttle between 0.0 and 1.0
+    pitch between -1.0 and 1.0
+    roll between -1.0 and 1.0
+    yaw between -1.0 and 1.0
+    """
+
+    ToReturn:bytearray = bytearray()
+
+    # Add header byte (metadata byte)
+    # bit 7, 6, 5, 4 are reserved (unused)
+    header:int = 0b00000000 # start with 0. Bit 0 must be 0 to identify as a control packet, which it already is!s
+    ToReturn.append(header)
+
+    # Add throttle bytes
+    asint16 = min(max(int(round(throttle * 65535, 0)), 0), 65535) # express as number between 0 and 65535
+    ToReturn.extend(asint16.to_bytes(2, "big"))
+
+    # add pitch bytes
+    aspop:float = (pitch + 1) / 2 # as percent of the range -1.0 to 1.0
+    asint16:int = min(max(int(round(aspop * 65535, 0)), 0), 65535) # convert the range from a number between 0 and 65535 (uint16)
+    ToReturn.extend(asint16.to_bytes(2, "big")) # pack as 2-bytes using big endian
+
+    # add roll bytes
+    aspop:float = (roll + 1) / 2 # as percent of the range -1.0 to 1.0
+    asint16:int = min(max(int(round(aspop * 65535, 0)), 0), 65535) # convert the range from a number between 0 and 65535 (uint16)
+    ToReturn.extend(asint16.to_bytes(2, "big")) # pack as 2-bytes using big endian
+
+    # add yaw bytes
+    aspop:float = (yaw + 1) / 2 # as percent of the range -1.0 to 1.0
+    asint16:int = min(max(int(round(aspop * 65535, 0)), 0), 65535) # convert the range from a number between 0 and 65535 (uint16)
+    ToReturn.extend(asint16.to_bytes(2, "big")) # pack as 2-bytes using big endian
+
+    # Add XOR-chain-based checksum
+    checksum:int = 0x00 # start with 0
+    for byte in ToReturn: # for each byte added so far
+        checksum = checksum ^ byte # XOR operation
+    ToReturn.append(checksum)
+
+    # return it
+    return bytes(ToReturn)
+
+
+
+
+
+
+
+
+
+
+
 # Lifted from the Scout Flight Controller, my previous work: https://github.com/TimHanewich/scout/blob/master/src/toolkit.py
 class NonlinearTransformer:
     """Converts a linear input to a nonlinear output (dampening) using tanh and a dead zone."""
