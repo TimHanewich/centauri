896ac8f092880727e87ce092a6089c1c6cfddbcd
Tim Hanewich <tahanewich@live.com>
2025-12-02 20:45:35 -0500

    Unpack joystick inpuit

--- components/controller/handheld/pico/src/tools.py
+++ components/controller/handheld/pico/src/tools.py
@@ -18,6 +18,35 @@ def unpack_button_input(packed:bytes) -> int: # returns the int that corresponds
 
     return btn_id
 
+def unpack_joystick_input(packed:bytes) -> tuple[int, float]: # unpacks as the ID of the joystick (see 'Joystick' Enum of the RPi's tools.py) and then the value as a floating point number
+
+    # if the length is not at least 3 (one header byte, two value bytes), there is a problem
+    if len(packed) < 3:
+        return None
+    
+    # define a binary mask of the bit positions we ONLY want
+    # we only want bits 0, 1, and 2. Those represent the numeric value
+    # bits 3, 4, 5, 6, and 7 are bits that contain other info
+    # We will isolate bits 0,1,2 and thus get the numeric value of the button, unaffected by those other info header bits
+    mask:int = 0b00000111
+
+    # Determine what joystick was moved
+    # this AND operation will only allow the bits that match up to be 1
+    # since we set bit 3-7 to 0, it will be impossible for those to come out as 1
+    joystick_id:int = packed[0] & mask
+
+    # Get the value
+    value:float = 0.0 # start w/ 0
+    if joystick_id == 4 or joystick_id == 5: # if it is the LT or RT, it is between 0.0 and 1.0
+        asuint16:int = packed[1] << packed[2]
+        value = asuint16 / 65535 # just a flat % of the total range!
+    else: # otherwise, it is the X/Y axis of the Left/Right stick... which can be between -1.0 and 1.0
+        asuint16:int = packed[1] << packed[2]
+        aspor:float = asuint16 / 65535 # % of total range
+        value = (2 * aspor) - 1.0 # restore to a -1.0 to 1.0
+
+    return (joystick_id, value)
+
 
 # Lifted from the Scout Flight Controller, my previous work: https://github.com/TimHanewich/scout/blob/master/src/toolkit.py
 class NonlinearTransformer:
