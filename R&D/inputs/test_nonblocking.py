376e4c0662c941fc071f964ea8418a2eb5a10e83
Tim Hanewich <tahanewich@live.com>
2025-12-04 10:42:51 -0500

    More elaborate test

--- R&D/inputs/test_nonblocking.py
+++ R&D/inputs/test_nonblocking.py
@@ -1,4 +1,13 @@
 from inputs import devices
+import time
 
+# Get list of gamepads
 gamepads = devices.gamepads
-print("Gamepads: " + str(gamepads))
+
+# select the controller we will use
+my_controller = gamepads[0]
+
+while True:
+    events = my_controller.read()
+    print("Events: " + str(events))
+    time.sleep(0.25)
