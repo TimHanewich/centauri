062d528f1798dcc9697afafa2b4e6c637b651af7
Tim Hanewich <tahanewich@live.com>
2025-12-07 07:48:55 -0500

    Focus on RT

--- R&D/inputs/test_BASIC.py
+++ R&D/inputs/test_BASIC.py
@@ -4,4 +4,7 @@ print("Not listening for events!")
 while True:
     events = get_gamepad()
     for event in events:
-        print("Event Type: " + str(event.ev_type) + ", Event Code: " + str(event.code) + ", Event State: " + str(event.state))
+        #print("Event Type: " + str(event.ev_type) + ", Event Code: " + str(event.code) + ", Event State: " + str(event.state))
+        if event.ev_type == "Absolute":
+            if event.code == "ABS_RZ":
+                print(str(event.state))
