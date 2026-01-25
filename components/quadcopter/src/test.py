aa694d72620f8a417b4eb3638e653825edc9b551
Tim Hanewich <tahanewich@live.com>
2026-01-24 23:18:25 -0500

    Deleted test script

--- components/quadcopter/src/test.py
+++
@@ -1,9 +0,0 @@
-f = open(r"C:\Users\timh\Downloads\tah\centauri\components\quadcopter\src\log", "rb")
-data = f.read()
-f.close()
-
-frames = data.split("\r\n".encode())
-
-for frame in frames:
-    if len(frame) > 0:
-        print(str(frame[18] * 10) + " ms")
