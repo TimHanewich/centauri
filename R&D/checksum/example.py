43b01a68398bc354635b7ecddbef78b60134d8e2
Tim Hanewich <tahanewich@live.com>
2026-02-14 09:00:21 -0500

    Updated checksum example for dual checksums

--- R&D/checksum/example.py
+++ R&D/checksum/example.py
@@ -1,11 +1,26 @@
-data = bytearray(b'\x00\xfdpW\n\x80\x00\x80\x00\xd0')
-data.pop(len(data)-1) # remove the checksum byte
+import time
+
+# construct "dummy" packet
+data = bytearray()
+data.append(0b00000000) # header byte
+data.append(253) # throttle byte 1
+data.append(112) # throttle byte 2
+data.append(87) # pitch byte 1
+data.append(10) # pitch byte 2
+data.append(128) # roll byte 1
+data.append(0) # roll byte 2
+data.append(128) # yaw byte 1
+data.append(0) # yaw byte 2
+
 
 # dual checksum
+t1 = time.ticks_us()
 checksum1:int = 0b00000000 # start with 0
 checksum2:int = 0b00000000 # start with 0
 for byte in data:
     checksum1 = checksum1 ^ byte
     checksum2 = checksum2 ^ checksum1
+t2 = time.ticks_us()
+print("Time, ticks us: " + str(t2 - t1))
 print("Checksum1: " + str(checksum1))
 print("Checksum2: " + str(checksum2))
