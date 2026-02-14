5ede60a14588e7875b1d6ffb41b2d8cd02921aaf
Tim Hanewich <tahanewich@live.com>
2026-02-14 08:52:30 -0500

    Optimized checksum:

--- forensic2.py
+++ forensic2.py
@@ -1,16 +1,11 @@
 data = bytearray(b'\x00\xfdpW\n\x80\x00\x80\x00\xd0')
 data.pop(len(data)-1) # remove the checksum byte
 
-
-# checksum 1
-checksum1:int = 0x00 # start with 0
-for byte in data: # for each byte added so far
-    checksum1 = checksum1 ^ byte # XOR operation
-
-# checksum2
-checksum2:int = 0x00 # start with 0
-for i in range(len(data)):
-    checksum2 = checksum2 ^ data[len(data)-1-i]
-
+# dual checksum
+checksum1:int = 0b00000000 # start with 0
+checksum2:int = 0b00000000 # start with 0
+for byte in data:
+    checksum1 = checksum1 ^ byte
+    checksum2 = checksum2 ^ checksum1
 print("Checksum1: " + str(checksum1))
 print("Checksum2: " + str(checksum2))
