e66780ad382f7b06b8bafc653c6cd0df90e41801
Tim Hanewich <tahanewich@live.com>
2026-07-08 15:20:25 -0400

    Put license preamble on all

--- components/quadcopter/calibrate/main.py
+++ components/quadcopter/calibrate/main.py
@@ -1,3 +1,22 @@
+# ==============================================================================
+# CENTAURI CALIBRATION SCRIPT
+# https://github.com/TimHanewich/centauri
+# ==============================================================================
+# Copyright (C) 2026 Tim Hanewich
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # this is a lightweight script to re-calibrate all of the motors on the quadcopter.
 # WARNING: DO THIS WITH PROPS OFF!
 
